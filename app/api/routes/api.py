72de44526b9e2edc5328f443fa8e7118732fa722
KrimsN_GK <deni.tim99@yandex.ru>
2021-10-30 22:00:39 +0700

    reformat

--- app/api/routes/api.py
+++ app/api/routes/api.py
@@ -4,7 +4,7 @@ from app.api.routes import (
     default,
     binance,
     coinbase,
-    kukoin,
+    kucoin,
     gate_io,
 )
 
@@ -14,5 +14,5 @@ api_router = APIRouter()
 api_router.include_router(default.router, tags=["default"])
 api_router.include_router(binance.router, tags=["Binance"], prefix='/binance')
 api_router.include_router(coinbase.router, tags=["Coinbase"], prefix='/coinbase')
-api_router.include_router(kukoin.router, tags=["KuCoin"], prefix='/kucoin')
+api_router.include_router(kucoin.router, tags=["KuCoin"], prefix='/kucoin')
 api_router.include_router(gate_io.router, tags=["Gate.io"], prefix='/gate_io')
