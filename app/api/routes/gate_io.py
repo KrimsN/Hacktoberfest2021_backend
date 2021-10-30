f8d854c98eff530d39b0d2954176144d97545cd1
KrimsN_GK <deni.tim99@yandex.ru>
2021-10-30 21:48:39 +0700

    gate_io

--- app/api/routes/gate_io.py
+++ app/api/routes/gate_io.py
@@ -11,3 +11,14 @@ async def get_trade_pairs_ids():
     return res
 
 
+@router.get("/avgPrice")
+async def get_avg_price(symbol: str):
+    res = GateIo.get_avg_price(symbol)
+    return res
+
+
+@router.get("/depth")
+async def get_price(symbol: str):
+    res = GateIo.get_depth(symbol)
+    return res
+
--- app/buisness_logic/gate_io.py
+++ app/buisness_logic/gate_io.py
@@ -1,18 +1,12 @@
 import requests
-import time
 from functools import lru_cache
 
+from app.buisness_logic.base_exchange import AbstractBaseExchange
 
-class GateIo:
+
+class GateIo(AbstractBaseExchange):
     _api_base = "https://api.gateio.ws"
-    _headers: dict[str, str] = {"Accept": "application/json"}
-
-    @staticmethod
-    def _add_meta_info(d: dict, symbol: str) -> dict:
-        d['symbol'] = symbol
-        d['platform'] = "gate.io"
-        d['timestamp_UTC+7'] = time.time()
-        return d
+    _platform_name: str = "gate.io"
 
     @classmethod
     @lru_cache
@@ -21,3 +15,34 @@ class GateIo:
         resp = requests.get(f"{cls._api_base}{path}", headers=cls._headers, allow_redirects=True).json()
         result = sorted([i['id'] for i in resp])
         return result
+
+    @classmethod
+    def get_avg_price(cls, symbol: str) -> dict:
+        resp = cls.get_depth(symbol)
+        res = {
+            "price": (float(resp['bids'][0]['price']) + float(resp['asks'][0]['price'])) / 2
+        }
+        res = cls._add_meta_info(res, symbol)
+        return res
+
+    @classmethod
+    def get_depth(cls, symbol: str) -> dict:
+        path: str = "/api/v4/spot/order_book"
+        query: str = f"?currency_pair={symbol}"
+        resp = requests.get(f"{cls._api_base}{path}{query}", allow_redirects=True, headers=cls._headers).json()
+
+        res_depth = {"bids": [], "asks": []}
+
+        for bid in resp["bids"]:
+            res_depth["bids"].append({
+                "price": bid[0],
+                "qty": bid[1]
+            })
+        for ask in resp["asks"]:
+            res_depth["asks"].append({
+                "price": ask[0],
+                "qty": ask[1]
+            })
+
+        res_depth = cls._add_meta_info(res_depth, symbol)
+        return res_depth
